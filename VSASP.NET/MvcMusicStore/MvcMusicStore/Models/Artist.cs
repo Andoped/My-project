a8cf354f3260dd91ac018db2e7fac42abd3c2374
Andoped <1196341787@qq.com>
2019-01-11 13:35:24 +0800

    Microsoft音乐商店

---
+++ VSASP.NET/MvcMusicStore/MvcMusicStore/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    //术家模型类
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ VSASP.NET/MvcMusicStore/MvcMusicStore/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    //流派类
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Album> Albums { get; set; }
+    }
+
+}
